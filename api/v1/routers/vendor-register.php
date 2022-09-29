eb88c51362d7bcea50b60e16ab8a7db4143fe332
sataporn1995 <satapornchai2538@gmail.com>
2022-09-29 16:30:48 +0700

    First commit

--- api/v1/routers/vendor-register.php
+++ api/v1/routers/vendor-register.php
@@ -5,6 +5,7 @@ require_once('../controllers/VendorRegister.controller.php');
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
      $req = (object) json_decode(file_get_contents('php://input'));
      $response = new VendorRegisterController($req);
+     echo json_encode($response);
 } else {
      http_response_code(400);
      $response = array("state" => false, "msg" => "Bad Request");
